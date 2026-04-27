2ee2d7040a76c98f39f60978547032511280c48e
makikatsume0503-source <maki.katsume.0503@gmail.com>
2026-04-27 18:07:05 +0900

    Insert AI introduction section on homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Hero } from './Hero';
+import { AIIntroduction } from './AIIntroduction';
 import { DigitalTwin } from './DigitalTwin';
 import { Demonstrations } from './Demonstrations';
 import { Profile } from './Profile';
@@ -10,6 +11,7 @@ export const Home: React.FC = () => {
     return (
         <>
             <Hero />
+            <AIIntroduction />
             <DigitalTwin />
             <Demonstrations />
             <Profile />
